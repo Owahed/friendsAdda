c1a039c85240aacfe2628fdb26da5cf3802cc685
Owahed Hossain <76661182+Owahed@users.noreply.github.com>
2021-05-29 11:50:38 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import Chat from "./Chat";
 import Sidebar from "./Sidebar";
 
 function App() {
-<
-  return (
-    <div className="app">
-      {/* Sidebar */}
-      <Sidebar/>
-      {/* chat df*/}
-  
-    </div>
-  );
-
-  
-
+    return (
+        <div className="app">
+            <Sidebar />
+            <Chat />
+        </div>
+    );
 }
 
 export default App;
